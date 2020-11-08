838ae8faa249f3157c1216cfe37a66591e95779c
oshble5151 <73323188+oshble5151@users.noreply.github.com>
2020-11-08 14:23:14 +0900

    Add files via upload

---
+++ promod.py
@@ -0,0 +1,12 @@
+import os ; os.chdir('./')
+def nctest(file:str):
+    import netCDF4 as nc
+    import numpy as np
+    nf = nc.Dataset(file,'r')
+    nml = nf.variables.keys()
+    for i in nml:
+        globals()[i] = nf.variables[i][:]
+
+
+nctest('atmos_month2001_0025_n.nc')
+print(tas)
