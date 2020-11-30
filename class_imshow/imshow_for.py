3656a94584001176d7f01e21c6fcf62f42be44ce
oshble5151 <73323188+oshble5151@users.noreply.github.com>
2020-11-30 14:53:41 +0900

    Add files via upload

---
+++ class_imshow/imshow_for.py
@@ -0,0 +1,47 @@
+import os
+os.chdir('/mnt/d/test')
+import netCDF4 as nc
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import docstring
+from matplotlib.pyplot import gcf,Axes,sci,cbook
+
+def gca(**kwargs):
+       return gcf().gca(**kwargs)
+       
+
+@docstring.copy(Axes.imshow)
+def imshow(self,
+    cmap='jet',
+    norm=None,
+    aspect=None,
+    interpolation=None,
+    alpha=None,
+    vmin=None,
+    vmax=None,
+    origin=None,
+    extent=None,
+    shape=cbook.deprecation._deprecated_parameter,
+    filternorm=1,
+    filterrad=4.0,
+    imlim=cbook.deprecation._deprecated_parameter,
+    resample=None,
+    url=None,
+    *,
+    data=None,
+    **kwargs):
+       __ret = gca().imshow(
+        self, cmap=cmap, norm=norm, aspect=aspect,
+        interpolation=interpolation, alpha=alpha, vmin=vmin,
+        vmax=vmax, origin=origin, extent=extent, shape=shape,
+        filternorm=filternorm, filterrad=filterrad, imlim=imlim,
+        resample=resample, url=url, **({"data": data} if data is not
+        None else {}), **kwargs)
+       sci(__ret)
+       plt.gca().invert_yaxis()
+       plt.show()
+
+       
+
+
+       
